e40f9759818dc3e04d445d03b55c813885a2ac09
Arun Kumar Patnana <arunkumar.vtalent@gmail.com>
2024-07-23 10:03:21 +0530

    A new controller created "EmpWebApi Controller"

---
+++ DemoWebAPISol/DemoWebAPI/Controllers/EmpWebApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpWebApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetNames")]
+        public IActionResult GetNames()
+        {
+            try
+            {
+                List<string> Names = new List<string>();
+                Names.Add("Virat");
+                Names.Add("Rohit");
+                Names.Add("Surya");
+                Names.Add("Jaddu");
+                Names.Add("Shikar");
+
+                return Ok(Names);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+    }
+}
